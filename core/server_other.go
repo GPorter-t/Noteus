1c0ed633d801a83a9267f4aad6b19ef1d4ec2653
GPorter-t <374744710@qq.com>
2022-08-04 18:29:51 +0800

    修改其他版本服务启动错误的情况

--- core/server_other.go
+++ core/server_other.go
@@ -6,9 +6,10 @@ package core
 import (
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
-func initServer(address string, rounter *gin.Engine) server {
+func initServer(address string, router *gin.Engine) server {
 	s := endless.NewServer(address, router)
 	s.ReadHeaderTimeout = 20 * time.Second
 	s.WriteTimeout = 20 * time.Second
