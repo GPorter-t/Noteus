f72139f7a0a74ef5e414956ffe0e2bdf2a98d2dd
GPorter-t <374744710@qq.com>
2022-08-06 16:20:18 +0800

    Nous LRU处调用协程进行管理

--- service/note/nous.go
+++ service/note/nous.go
@@ -17,18 +17,20 @@ func (s *NousService) GetAll() (keys []string, err error) {
 }
 
 func (s *NousService) GetItem(key string) (id string, value string, err error) {
-	if global.GVA_STORE.LRU.Len("notes:nous") == 0 {
-		res, e := s.GetAll()
-		if e != nil {
-			err = e
-			return
+	go func() {
+		if global.GVA_STORE.LRU.Len("notes:nous") == 0 {
+			res, e := s.GetAll()
+			if e != nil {
+				err = e
+				return
+			}
+			for i := 0; i < global.GVA_CONFIG.System.LruMaxSize; i++ {
+				index := rand.Intn(len(res))
+				v, _ := global.GVA_REDIS.HGet(ctx, "notes:nous", res[index]).Result()
+				global.GVA_STORE.LRU.Add("notes:nous", res[index], v)
+			}
 		}
-		for i := 0; i < global.GVA_CONFIG.System.LruMaxSize; i++ {
-			index := rand.Intn(len(res))
-			v, _ := global.GVA_REDIS.HGet(ctx, "notes:nous", res[index]).Result()
-			global.GVA_STORE.LRU.Add("notes:nous", res[index], v)
-		}
-	}
+	}()
 
 	if key == "" {
 		k, v, ok := global.GVA_STORE.LRU.GetBack("notes:nous")
